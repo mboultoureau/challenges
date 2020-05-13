f9bcd0d9345accb77d981f6a84e382485565659a
mboultoureau <contact@mboultoureau.com>
2020-05-14 00:56:57 +0200

    Day 1 - Added Data Types

---
+++ 30-days-of-code/day1-data-types/data-types.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+integer = 0
+double = 0.0
+string = ""
+
+integer = int(input())
+double = float(input())
+string = input()
+
+print(i + integer)
+print(d + double)
+print(s + string)
